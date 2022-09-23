6a6c827c54712c22fa91e435f8368e45d9c733c8
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-09-23 20:07:18 +0300

    Add files via upload

---
+++ convert_log_to_mol2.sh
@@ -0,0 +1,9 @@
+#Install antechamber by using this command :antechamber -i "$mol".mol2 -fi mol2 -o "$mol".pdb -fo pdb -pf yes
+# or run this to install antechamber:   conda install -c "conda-forge/label/cf202003" acpype 
+while IFS=" " read -r mol;
+do
+# convert gaussian log files to mol2
+antechamber -i ../"$mol".log -fi gout -o "$mol".pdb -fo pdb -pf yes
+# convert mol2 format to pdbm format
+#antechamber -i "$mol".mol2 -fi mol2 -o "$mol".pdb -fo pdb -pf yes
+done < list2_3
